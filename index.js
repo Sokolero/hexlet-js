9782373ae1b0ae9c6236e6926d30f8f5c8f7207e
sokolero <sokolero@yandex.com>
2020-09-14 18:47:44 +0300

    Add eslint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// var _ = require('lodash');
+import _ from 'lodash';
 
-import _ from "lodash";
-
-console.log("Первый проект запущен!");
-console.log(_.last(["one", "two"]));
+console.log('Первый проект запущен!');
+console.log(_.last(['one', 'two']));
 
 const fn = (a, b) => {
   const c = a + b;
   return c;
 };
+
+console.log(fn(5, 9));
